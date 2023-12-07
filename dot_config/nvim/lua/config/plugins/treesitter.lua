98afb4b0683b0c6bbb57204101a81598a50308dd
Mathieu Lemay <acidrain1@gmail.com>
2023-12-07 08:01:55 -0500

    nvim: Enable terraform lsp and treesitter

--- dot_config/nvim/lua/config/plugins/treesitter.lua
+++ dot_config/nvim/lua/config/plugins/treesitter.lua
@@ -2,9 +2,11 @@ require("nvim-treesitter.configs").setup({
     ensure_installed = {
         "bash",
         "dockerfile",
+        "hcl",
         "json",
         "lua",
         "python",
         "rust",
+        "terraform",
     },
 })
