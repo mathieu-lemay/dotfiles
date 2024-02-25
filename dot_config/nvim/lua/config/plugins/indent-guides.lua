8c36697ac51b630059eaa5ea6275b0ab4c1a8440
Mathieu Lemay <acidrain1@gmail.com>
2024-02-25 15:31:21 -0500

    nvim: Update plugins and disable indent guide for first level

--- dot_config/nvim/lua/config/plugins/indent-guides.lua
+++ dot_config/nvim/lua/config/plugins/indent-guides.lua
@@ -1,4 +1,23 @@
+local indent_char = "│"
+
 require("ibl").setup({
-    indent = { char = "│" },
-    scope = { show_start = false, show_end = false, char = "│" },
+    indent = {
+        char = indent_char,
+    },
+    scope = {
+        show_start = false,
+        show_end = false,
+        char = indent_char,
+    },
 })
+
+hooks = require("ibl.hooks")
+-- Workaround to disable the first level
+-- source: https://github.com/lukas-reineke/indent-blankline.nvim/issues/824
+hooks.register(hooks.type.VIRTUAL_TEXT, function(_, _, _, virt_text)
+    if virt_text[1] and virt_text[1][1] == indent_char then
+        virt_text[1] = { " ", { "@ibl.whitespace.char.1" } }
+    end
+
+    return virt_text
+end)
