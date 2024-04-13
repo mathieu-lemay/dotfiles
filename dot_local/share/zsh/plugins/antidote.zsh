de25c069ddafad0a1410479eeb0031b7e6636f99
Mathieu Lemay <acidrain1@gmail.com>
2024-04-13 13:36:52 -0400

    zsh: Replace zinit by antidote

---
+++ dot_local/share/zsh/plugins/antidote.zsh
@@ -0,0 +1,14 @@
+_antidote_dir="${HOME}/.local/share/zsh-antidote"
+
+[[ -e "${_antidote_dir}" ]] || return
+
+_antidote_init_script="${_antidote_dir}/antidote/antidote.zsh"
+_antidote_plugins_file="${_antidote_dir}/plugins.txt"
+_antidote_bundle_file="${_antidote_dir}/plugins.zsh"
+
+if [[ ! -e "${_antidote_bundle_file}" ]] || [[ "${_antidote_plugins_file}" -nt "${_antidote_bundle_file}" ]]; then
+    source "${_antidote_init_script}"
+    antidote bundle <"${_antidote_plugins_file}" >"${_antidote_bundle_file}"
+fi
+
+source "${_antidote_bundle_file}"
