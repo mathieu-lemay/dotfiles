290d75c17d6e9b5919777323fd6b063529130048
Mathieu Lemay <acidrain1@gmail.com>
2024-05-27 16:01:06 -0400

    nvim: Switch to phha/zenburn.nvim

--- dot_config/nvim/lua/config/colors.lua
+++ dot_config/nvim/lua/config/colors.lua
@@ -2,11 +2,11 @@ local nb_colors = vim.fn.system("tput colors")
 nb_colors = nb_colors:gsub("%s+", "")
 
 if nb_colors == "256" then
-    vim.opt.termguicolors = true
-    vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+    require("zenburn").setup()
 
-    vim.g.zenburn_italic_Comment = 1
-    vim.cmd.colorscheme("zenburn")
+    comment_hl = vim.api.nvim_get_hl(0, { name = "Comment" })
+    comment_hl.italic = true
+    vim.api.nvim_set_hl(0, "Comment", comment_hl)
 else
     vim.cmd.colorscheme("desert")
 end
