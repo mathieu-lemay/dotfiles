9e3de8a753706433aec3c1025232e78a16f1e873
Mathieu Lemay <acidrain1@gmail.com>
2024-06-05 19:09:22 -0400

    macOS: Fix location of homebrew
    Homebrew used to install in /usr/local, it is now in /opt/homebrew

--- dot_local/share/zsh/plugins/brew.zsh
+++ dot_local/share/zsh/plugins/brew.zsh
@@ -2,18 +2,22 @@ if ! installed brew; then
     return
 fi
 
-BREW_PREFIX="$(brew --prefix)"
-export PATH="$PATH:${BREW_PREFIX}/sbin"
+HOMEBREW_PREFIX="$(brew --prefix)"
+HOMEBREW_CELLAR="${HOMEBREW_PREFIX}/Cellar";
+
+export PATH="${HOMEBREW_PREFIX}/bin:${HOMEBREW_PREFIX}/sbin${PATH+:$PATH}";
+export MANPATH="${HOMEBREW_PREFIX}/share/man${MANPATH+:$MANPATH}:";
+export INFOPATH="${HOMEBREW_PREFIX}/share/info:${INFOPATH:-}";
 
 # GNU utilities
 for pkg in "coreutils" "findutils" "gnu-sed"; do
-    gnubin="${BREW_PREFIX}/opt/${pkg}/libexec/gnubin"
+    gnubin="${HOMEBREW_PREFIX}/opt/${pkg}/libexec/gnubin"
     [[ -d "${gnubin}" ]] && export PATH="${gnubin}:${PATH}"
 done
 
 # Lib specific flags
 for lib in 'openssl' 'zlib'; do
-    lib_path="${BREW_PREFIX}/opt/${lib}"
+    lib_path="${HOMEBREW_PREFIX}/opt/${lib}"
     if [[ -d "${lib_path}" ]]; then
         export LDFLAGS="$LDFLAGS -L${lib_path}/lib"
         export CFLAGS="$CFLAGS -I${lib_path}/include"
@@ -22,7 +26,7 @@ for lib in 'openssl' 'zlib'; do
 done
 
 # Qt
-QT_PATH="${BREW_PREFIX}/opt/qt"
+QT_PATH="${HOMEBREW_PREFIX}/opt/qt"
 if [[ -d "${QT_PATH}" ]]; then
     export CMAKE_PREFIX_PATH="${QT_PATH}:$CMAKE_PREFIX_PATH"
     export PATH="$PATH:${QT_PATH}/bin"
