07cd5ec04092c0727895a31f532e7f1bcb611fa1
Mathieu Lemay <acidrain1@gmail.com>
2024-10-15 10:42:34 -0400

    python: Improve histfile detection, remove redundant var

--- dot_config/python/init.py
+++ dot_config/python/init.py
@@ -1,31 +1,34 @@
 import atexit
 import os
 import readline
+from pathlib import Path
 
 
-def _get_hist_file():
+def _get_hist_file() -> Path:
     if "PYTHONHISTFILE" in os.environ:
-        history = os.environ["PYTHONHISTFILE"]
+        history = Path(os.environ["PYTHONHISTFILE"])
     elif "XDG_STATE_HOME" in os.environ:
-        history = os.path.join(os.environ["XDG_STATE_HOME"], "python", "history")
+        history = Path(os.environ["XDG_STATE_HOME"]) / "python" / "history"
     else:
-        history = os.path.join("~", ".python_history")
+        history = (Path("~") / "python")
 
-    history = os.path.abspath(os.path.expanduser(history))
-    _dir, _ = os.path.split(history)
-    os.makedirs(_dir, exist_ok=True)
+    history = history.absolute().expanduser()
+
+    if not history.exists():
+        history.parent.mkdir(parents=True, exist_ok=True)
+        # Write a dummy history to avoid an operation not permitted error
+        history.write_text("_HiStOrY_V2_\nexit()\n")
 
     return history
 
-
-def _setup_history():
+def _setup_history() -> None:
     hist_file = _get_hist_file()
 
     try:
         readline.read_history_file(hist_file)
         # default history len is -1 (infinite), which may grow unruly
         readline.set_history_length(10000)
-    except FileNotFoundError:
+    except IOError:
         pass
 
     atexit.register(readline.write_history_file, hist_file)
