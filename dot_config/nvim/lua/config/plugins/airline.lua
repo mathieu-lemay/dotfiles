e06ac1e7f7f018691137d9a1975f00c0e2c57f88
Mathieu Lemay <acidrain1@gmail.com>
2025-02-20 11:52:04 -0500

    nvim: Add temporary workaround for airline / utf8 issue

--- dot_config/nvim/lua/config/plugins/airline.lua
+++ dot_config/nvim/lua/config/plugins/airline.lua
@@ -11,3 +11,8 @@ vim.g.airline_left_alt_sep = ">"
 vim.g.airline_right_alt_sep = "<"
 
 vim.g.airline_section_z = "%#__accent_bold#%4l/%L%#__restore__# :%3v"
+
+-- Workaround for:
+--   https://github.com/neovim/neovim/issues/31956
+--   https://github.com/vim-airline/vim-airline/issues/2704
+vim.g["airline#extensions#whitespace#symbol"] = '!'
