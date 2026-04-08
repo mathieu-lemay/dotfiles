008300cc6462a7e4b965024cc9c26ed562f7bd00
Mathieu Lemay <acidrain1@gmail.com>
2026-04-08 10:41:50 -0400

    nvim: Use main branch for nvim-treesitter

--- dot_config/nvim/lua/config/plugins/treesitter.lua
+++ dot_config/nvim/lua/config/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-require("nvim-treesitter.configs").setup({
+require("nvim-treesitter.config").setup({
     ensure_installed = {
         "bash",
         "dockerfile",
@@ -10,6 +10,7 @@ require("nvim-treesitter.configs").setup({
         "rust",
         "sql",
         "terraform",
+        "yaml",
     },
 
     highlight = {
@@ -32,8 +33,34 @@ require("nvim-treesitter.configs").setup({
     },
 })
 
--- Use treesitter as folding method
-vim.o.foldmethod = 'expr'
-vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.o.foldlevelstart = 99
 vim.o.foldnestmax = 2
+
+-- on main branch, treesitter isn't started automatically
+vim.api.nvim_create_autocmd({ 'Filetype' }, {
+  callback = function(event)
+    -- make sure nvim-treesitter is loaded
+    local ok, nvim_treesitter = pcall(require, 'nvim-treesitter')
+
+    -- no nvim-treesitter, maybe fresh install
+    if not ok then return end
+
+    local parsers = require('nvim-treesitter.parsers')
+
+    if not parsers[event.match] or not nvim_treesitter.install then return end
+
+    local ft = vim.bo[event.buf].ft
+    local lang = vim.treesitter.language.get_lang(ft)
+    nvim_treesitter.install({ lang }):await(function(err)
+      if err then
+        vim.notify('Treesitter install error for ft: ' .. ft .. ' err: ' .. err)
+        return
+      end
+
+      pcall(vim.treesitter.start, event.buf)
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.wo.foldmethod = 'expr'
+    end)
+  end,
+})
