a91f689b04cc4d8063853e1a2b5c1bc3e63add3a
Mathieu Lemay <acidrain1@gmail.com>
2026-04-11 10:31:38 -0400

    nvim: Pre-install treesitter parsers

--- dot_config/nvim/lua/config/plugins/treesitter.lua
+++ dot_config/nvim/lua/config/plugins/treesitter.lua
@@ -1,68 +1,60 @@
-require("nvim-treesitter.config").setup({
-    ensure_installed = {
-        "bash",
-        "dockerfile",
-        "go",
-        "hcl",
-        "json",
-        "lua",
-        "python",
-        "rust",
-        "sql",
-        "terraform",
-        "yaml",
-    },
+local parsers = {
+    "bash",
+    "dockerfile",
+    "go",
+    "hcl",
+    "json",
+    "just",
+    "lua",
+    "make",
+    "python",
+    "rust",
+    "sql",
+    "terraform",
+    "yaml",
+}
 
-    highlight = {
-        enable = true,
-
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-            local max_filesize = 1024 * 1024 -- 1 MiB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
-        end,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
-})
+-- Install parsers after startup
+vim.schedule(function()
+    require("nvim-treesitter").install(parsers)
+end)
 
 vim.o.foldlevelstart = 99
 vim.o.foldnestmax = 2
 
 -- on main branch, treesitter isn't started automatically
-vim.api.nvim_create_autocmd({ 'Filetype' }, {
-  callback = function(event)
-    -- make sure nvim-treesitter is loaded
-    local ok, nvim_treesitter = pcall(require, 'nvim-treesitter')
+vim.api.nvim_create_autocmd({ "Filetype" }, {
+    callback = function(event)
+        -- make sure nvim-treesitter is loaded
+        local ok, nvim_treesitter = pcall(require, "nvim-treesitter")
 
-    -- no nvim-treesitter, maybe fresh install
-    if not ok then return end
+        -- no nvim-treesitter, maybe fresh install
+        if not ok then
+            return
+        end
 
-    local parsers = require('nvim-treesitter.parsers')
+        local parsers = require("nvim-treesitter.parsers")
 
-    if not parsers[event.match] or not nvim_treesitter.install then return end
+        if not parsers[event.match] or not nvim_treesitter.install then
+            return
+        end
 
-    local ft = vim.bo[event.buf].ft
-    local lang = vim.treesitter.language.get_lang(ft)
-    nvim_treesitter.install({ lang }):await(function(err)
-      if err then
-        vim.notify('Treesitter install error for ft: ' .. ft .. ' err: ' .. err)
-        return
-      end
+        local ft = vim.bo[event.buf].ft
+        local lang = vim.treesitter.language.get_lang(ft)
+        nvim_treesitter.install({ lang }):await(function(err)
+            if err then
+                vim.notify("Treesitter install error for ft: " .. ft .. " err: " .. err)
+                return
+            end
 
-      pcall(vim.treesitter.start, event.buf)
-      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      if not vim.opt.diff:get() then
-        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        vim.wo.foldmethod = 'expr'
-      end
-    end)
-  end,
+            pcall(vim.treesitter.start, event.buf)
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+            -- Use treesitter for folds when not in diff mode
+            if not vim.opt.diff:get() then
+                vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                vim.wo.foldmethod = "expr"
+            end
+        end)
+    end,
 })
