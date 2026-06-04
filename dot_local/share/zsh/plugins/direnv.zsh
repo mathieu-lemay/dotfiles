a4b3a2844fd6f2fae909a0138a2f53fb0a888013
Mathieu Lemay <acidrain1@gmail.com>
2026-06-04 19:16:12 -0400

    zsh: Add functions to temporarily disable/re-enable direnv

--- dot_local/share/zsh/plugins/direnv.zsh
+++ dot_local/share/zsh/plugins/direnv.zsh
@@ -1,3 +1,13 @@
 command -v direnv &> /dev/null || return
 
-eval "$(direnv hook zsh)"
+direnv-disable() {
+    unset -f _direnv_hook
+
+    eval "$(cd /tmp; direnv export zsh)"
+}
+
+direnv-enable() {
+    eval "$(direnv hook zsh)"
+}
+
+direnv-enable
